05cd989ad84b937da6c18748c0ddf2e2faa4987a
Animorphs <jakejuretic@hotmail.com>
2018-09-04 20:49:37 +1000

    connect.php works now
    Updated connect.php so it actually connects to the database, not a local database

--- scripts/connect.php
+++ scripts/connect.php
@@ -1,15 +1,15 @@
 <?php
-/*
+
 $hostname = 'achEng.asdougl.com';
 $username = 'asdouglc_acheng';
 $password = '1Fl5L5HUeF';
 $database = 'asdouglc_achievementEngine';
-*/
-
+/*
 $hostname = 'localhost';
 $username = 'root';
 $password = '';
 $database = 'login';
+*/
 
 // connect to the database
 $db = new mysqli($hostname, $username, $password, $database);
