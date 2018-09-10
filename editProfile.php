53ad376d8fd6d70929f8332f8dd02d1484a152ff
Rephot <41567831+Rephot@users.noreply.github.com>
2018-09-10 15:53:29 +1000

    Added website icon
    Added our teamae icon as the website favicon (icon shown on web tab)

--- editProfile.php
+++ editProfile.php
@@ -32,6 +32,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="icon" href="data/teamae.png">
 	<link rel="stylesheet" type="text/css" href="data/style.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
