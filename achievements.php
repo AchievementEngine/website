32063d7cbd34c67673f4f67887529273953d5435
Animorphs <jakejuretic@hotmail.com>
2018-09-21 01:09:04 +1000

    Formatting of achievements.php
    Made it way way way more readable

--- achievements.php
+++ achievements.php
@@ -9,50 +9,17 @@
 	$row = $results->fetch_array(MYSQLI_ASSOC);
 	$dispName = $row['dispName'];
 	
-	$gameNames = array();	//game names
-	$gameIDs = array();		//game id
-	$gamesExist = true;		//games exist is true
-	$ugames = "SELECT g.gameName, g.gameID FROM ugames u INNER JOIN games g on u.gameID = g.gameID WHERE u.username = '$username'";
-	$ugamesResults = $db->query($ugames);
+	$gamesExist = true;
+	$userGames = array();	//array to store game IDs that user has
+	$userGameQuery = "SELECT * FROM games g JOIN ugames ug ON g.gameID = ug.gameID WHERE username = '$username'";
+	$ugamesResults = $db->query($userGameQuery);
 	if ($ugamesResults->num_rows > 0) {
 		while ($ugameRow = $ugamesResults->fetch_assoc()) {
-			array_push($gameIDs, $ugameRow['gameID']);
-			array_push($gameNames, $ugameRow['gameName']);
-		} 
+			array_push($userGames, $ugameRow['gameID']);
+		}
 	} else {
 		$gamesExist = false;	//set to no games existing
 	}
-	
-	$gameNameAndAchs = array();	//stores game names and achievements unlocked by the current user
-	$achsLocked = array();	//game name unlocked, but achievements locked by user
-	foreach($gameIDs as $gameID) { 
-		$game = "SELECT * FROM games WHERE gameID='$gameID'";
-		$gameResults = $db->query($game);
-		while ($gameRow = $gameResults->fetch_assoc()) {
-			$gameNameAndAchs[$gameRow['gameName']] = array();	//this currently stores the game names, each with their own empty array
-			$achsLocked[$gameRow['gameName']] = array();	//same
-		}
-	}
-	
-	foreach ($gameNameAndAchs as $gameName => $value) {
-		$unlockedAch = "SELECT * FROM uachievements u INNER JOIN achievements a ON u.achStr = a.achStr AND u.gameID = a.gameID INNER JOIN games g ON a.gameID = g.gameID WHERE u.username='$username' AND g.gameName = '$gameName' AND u.progress = a.achValue";
-		$lockedAch = "SELECT * FROM uachievements u INNER JOIN achievements a ON u.achStr = a.achStr AND u.gameID = a.gameID INNER JOIN games g ON a.gameID = g.gameID WHERE u.username='$username' AND g.gameName = '$gameName' AND u.progress != a.achValue";
-		$unlockedAchResults = $db->query($unlockedAch);
-		$lockedAchResults = $db->query($lockedAch);
-		if ($unlockedAchResults == true) {
-			while ($unlockedAchRow = $unlockedAchResults->fetch_assoc()) {
-				array_push($gameNameAndAchs[$gameName], $unlockedAchRow['achName']);		
-				//this adds each games achievements to their array. It's a multidimensional array,
-				//e.g. gameNameAndAchs(game1(ach1, ach2, ach3), game2(ach1, ach2, ach3));
-			}
-		}
-		if ($lockedAchResults == true) {
-			while ($lockedAchRow = $lockedAchResults->fetch_assoc()) {
-				array_push($achsLocked[$gameName], $lockedAchRow['achName']);
-			}
-		}
-	}
-	
 	//check user is logged in
 	if (!isset($_SESSION['username'])) {
 		$_SESSION['msg'] = "You must log in first";
@@ -80,12 +47,14 @@
 	<div class="gameScroll">
 		<div class="scrollbar" id="style-1">
 			<div class="btn-group">
-				<?php foreach($gameNameAndAchs as $game => $ach) { 
-					$games = "SELECT * FROM games WHERE gameName = '$game'";
+				<?php foreach($userGames as $gameID) { 
+					$games = "SELECT * FROM games WHERE gameID = '$gameID'";
 					$gameResults = $db->query($games);
 					$gameRow = $gameResults->fetch_array(MYSQLI_ASSOC);
 					$gameStr = $gameRow['gameStr'];
-					echo "<button onclick='$gameStr()' id='$game'>$game</button>"; 
+					$gameName = $gameRow['gameName'];
+					//echo "test";
+					echo "<button onclick='$gameStr()' id='$gameName'>$gameName</button>"; 
 				} ?>
 			</div>
 			
@@ -117,97 +86,79 @@
 </body>
 
 <script>
-	<?php foreach($gameNameAndAchs as $game => $ach) { 
-		$games = "SELECT * FROM games WHERE gameName = '$game'";
+	<?php foreach($userGames as $gameID) { 	//for each game, make a function, to display locked and unlocked achs
+		$games = "SELECT * FROM games WHERE gameID = '$gameID'";
 		$gameResults = $db->query($games);
 		$gameRow = $gameResults->fetch_array(MYSQLI_ASSOC);
 		$gameStr = $gameRow['gameStr'];
-		?>
-		function <?php echo $gameStr; ?>() {
-			
-			var unlocked = document.getElementById("unlocked");
+		$gameName = $gameRow['gameName']; ?>
+		function <?php echo $gameStr; ?>() {	//unlocked achievements			
+			var unlocked = document.getElementById("unlocked");	 
+			var locked = document.getElementById("locked");	
+			var header = document.getElementById("myDIVheader");
 			if (unlocked.style.display === "none") {	//unhide div when game clicked
 				unlocked.style.display = "block";
 			}
-			
-			document.getElementById("myDIVheader").innerHTML = "<?php echo $game?>";	//game title
-			
-			document.getElementById("unlocked").innerHTML = "<?php 
-				echo 'Unlocked:';
-				echo '<br><br>';
-				foreach($ach as $c => $achName) {
-					$achQ = "SELECT * FROM games g JOIN achievements a ON a.gameID = g.gameID WHERE gameName = '$game' AND achName = '$achName'";
-					$achR = $db->query($achQ);
-					while ($achRow = $achR->fetch_assoc()) {
-						$achStr = $achRow['achStr'];
-						$achDesc = $achRow['achDesc'];
-						$achPic = "data/achievements/".$gameStr."/".$achStr.".png";
-						echo "<img src=".$achPic." height='70px'>";
-						echo $achName."&emsp;&emsp;&emsp;";
-						echo $achDesc."&emsp;&emsp;&emsp;";
-						echo "<br><br>";
-					}
-				}	
-			?>";
-			<?php echo $gameStr."_Locked"; ?>();	//call function to print locked achievements
-		}
-	<?php } ?>	
-	
-	<?php foreach($achsLocked as $game => $ach) { 
-		$games = "select * from games where gameName = '$game'";
-		$gameResults = $db->query($games);
-		$gameRow = $gameResults->fetch_array(MYSQLI_ASSOC);
-		$gameStr = $gameRow['gameStr'];
-		?>
-		function <?php echo $gameStr."_Locked"; ?>() {
-			var locked = document.getElementById("locked");
-			if (locked.style.display === "none") {	//unhide div when game clicked
-				locked.style.display = "block";
+			if (locked.style.display === "none") {	
+					locked.style.display = "block";
 			}
-			document.getElementById("myDIVheader").innerHTML = "<?php echo $game?>";	//game title
 			
-			<?php if (empty($ach)) { ?>		//if all achievements are unlocked, hide Locked div, and replace "unlocked" with "all achievements unlocked"
-				if (locked.style.display === "block") {	//unhide div when game clicked
-					locked.style.display = "none";
-				}
-				$('#unlocked').contents().filter(function() {
-					return this.nodeType == 3
-				}).each(function(){
-					this.textContent = this.textContent.replace('Unlocked:','All achievements unlocked:');
-				});
-			<?php } ?>
-			
-			document.getElementById("locked").innerHTML = "<?php 
-				echo 'Locked:<br><br>';
-				foreach($ach as $c => $achName) {	//c is array index (0, 1, etc), d is the achievement name
-					$query = "SELECT * FROM uachievements u INNER JOIN achievements a ON u.achStr = a.achStr AND u.gameID = a.gameID INNER JOIN games g ON a.gameID = g.gameID WHERE u.username='$username' AND g.gameName = '$game' AND a.achName = '$achName'";
-					$queryResults = $db->query($query);
-					while ($queryRow = $queryResults->fetch_assoc()) {
-						$progress = $queryRow['progress'];
-						$achValue = $queryRow['achValue'];
-						$achDesc = $queryRow['achDesc'];
-						$achStr = $queryRow['achStr'];
-						$gameStr = $queryRow['gameStr'];
-					}
-					$achPic = "data/achievements/".$gameStr."/".$achStr.".png";
-					echo "<img src=".$achPic." height='70px'> <br>";
-					echo $achName."&emsp;&emsp;&emsp;";
-					echo $achDesc."&emsp;&emsp;&emsp;";
-					echo "Progress: ".$progress."/".$achValue."<br>";
-					$progressPercent = ($progress/$achValue) * 100;
+			document.getElementById("unlocked").innerHTML = "<h1 style='font-size:18pt'> Unlocked Achievements </h1> <br>";		//refresh for new game
+			document.getElementById("locked").innerHTML = "<h1 style='font-size:18pt'> Locked Achievements </h1> <br>"; 
+			<?php
+			$achievements = "SELECT * FROM uachievements ua JOIN achievements a ON ua.gameID = a.gameID WHERE username = '$username' AND a.gameID = '$gameID' and ua.achStr = a.achStr and ua.progress = a.achValue";
+			$achResults = $db->query($achievements);		
+			while ($achRow = $achResults->fetch_assoc()) {
+				$achName = $achRow['achName'];
+				$achDesc = $achRow['achDesc'];
+				$achStr = $achRow['achStr'];
+				$achPic = "data/achievements/".$gameStr."/".$achStr.".png";
+				?>
+				header.innerHTML = "<?=$gameName?> ";
+				
+				unlocked.innerHTML += 						
+					"<div>" +
+						"<img src='<?=$achPic?>' height='70px'>" +			//this is the image
+					"</div>" +
 					
-					echo "<div class='achievement-progress'>";
-						echo "<div class='pBar' style='width:30%; height:20px; border-radius: 10px;  margin:0 auto;'>";
-							echo "<div class='pProgress' style='width:".$progressPercent."%; height:10px; border-radius: 10px;'>".$progressPercent."%</div>";
-						echo "</div>";
-					echo "</div>";
-					echo "<br>";
-				}	
-			?>";
+					"<div>" +
+						"<?=$achName?>" +									//this is the name of the achievmeent
+					"</div>" +
+					
+					"<div>" +
+						"<?=$achDesc?>" +									//this is the description of the achievement
+					"</div>" +
+					
+					"<br>";
+				
+			<?php
+			}
+			$lockedAchievements = "SELECT * FROM uachievements ua JOIN achievements a ON ua.gameID = a.gameID WHERE username = '$username' AND a.gameID = '$gameID' AND ua.achStr = a.achStr AND ua.progress < a.achValue";
+			$lockedResults = $db->query($lockedAchievements);	
+			while ($lockedRow = $lockedResults->fetch_assoc()) {
+				$lockedName = $lockedRow['achName'];
+				$lockedDesc = $lockedRow['achDesc'];
+				$lockedStr = $lockedRow['achStr'];
+				$lockedPic = "data/achievements/".$gameStr."/".$lockedStr.".png";
+				?>
+				locked.innerHTML += 					
+					"<div>" +
+						"<img src='<?=$lockedPic?>' height='70px'>" +			//this is the image
+					"</div>" +
+					
+					"<div>" +
+						"<?=$lockedName?>" +									//this is the name of the achievmeent
+					"</div>" +
+					
+					"<div>" +
+						"<?=$lockedDesc?>" +									//this is the description of the achievement
+					"</div>" +
+					
+					"<br>";
+				
+			<?php }	?>	
 		}
-	<?php } ?>	
+	<?php } ?>
 </script>
-
 <?php include ('include/profileDropdown.php'); ?>
-
 </html>
