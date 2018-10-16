7a41f2c00745fcaf7b6e65b99c0ea14bd7d320a1
Rephot <41567831+Rephot@users.noreply.github.com>
2018-10-16 17:21:37 +1100

    Search Buttom Fixed
    I've done some styling changes to make the search button look good

--- include/header2.php
+++ include/header2.php
@@ -9,7 +9,7 @@
 		<form method="post" action="search.php">
 			<div class="searchzone">
 				<input type="text" name="search" class="searchBox" placeholder="Search users...">
-				<button style="font-family:FontAwesome;height:40px;width:40px;border-radius:10%"><i class="fas fa-search"></i></button>
+				<button style="font-family:FontAwesome;height:42px;width:70px;border-radius:10%; background-color: grey; border: 0"><i class="fas fa-search"></i></button>
 			</div>
 		</form>
 	</div>
