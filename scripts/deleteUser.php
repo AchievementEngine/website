99eb82db1e0704006ab0dd5d04ac4d15bbeda6b7
Animorphs <jakejuretic@hotmail.com>
2018-10-17 21:09:44 +1100

    Updated delete user
    Stopped working properly when messaging was introduced, but now fixed

--- scripts/deleteUser.php
+++ scripts/deleteUser.php
@@ -12,17 +12,27 @@
 		$admin2Results = $db->query($userAdmin2);
 		while ($admin1Row = $admin1Results->fetch_assoc()) {
 			$admin1 = $admin1Row['admin'];
-		} 
+		}
 		while ($admin2Row = $admin2Results->fetch_assoc()) {
 			$admin2 = $admin2Row['admin'];
-		} 
-		if ($admin1 > $admin2) { 		
+		}
+		if ($admin1 > $admin2) { 
+			$deleteUserMessages = "SELECT * FROM conversation WHERE sUsername = '$username2' OR rUsername = '$username2'";
+			$messageUserResults = $db->query($deleteUserMessages);
+			while ($messageUserRow = $messageUserResults->fetch_assoc()) {
+				$userid = $messageUserRow['id'];
+				$deleteUserMessages2 = "DELETE FROM messages WHERE id = '$userid'";
+				$db->query($deleteUserMessages2);
+			}
+			
+			$deleteUser0 = "DELETE FROM conversation WHERE sUsername = '$username2' OR rUsername = '$username2'";
 			$deleteUser1 = "DELETE FROM ugames WHERE username = '$username2'";
 			$deleteUser2 = "DELETE FROM uachievements WHERE username = '$username2'";
 			$deleteUser3 = "DELETE FROM friendrequest WHERE user1 = '$username2' OR user2 = '$username2'";
 			$deleteUser4 = "DELETE FROM friends WHERE user1 = '$username2' OR user2 = '$username2'";
 			$deleteUser5 = "DELETE FROM users WHERE username = '$username2'";
 			
+			$db->query($deleteUser0);
 			$db->query($deleteUser1);
 			$db->query($deleteUser2);
 			$db->query($deleteUser3);
@@ -34,12 +44,23 @@
 	
 	if(isset($_GET['deleteSelf'])) {
 		$username = $_SESSION['username'];
+		
+		$deleteMessages = "SSELECT * FROM conversation WHERE sUsername = '$username' OR rUsername = '$username'";
+		$messageResults = $db->query($deleteMessages);
+		while ($messageRow = $messageResults->fetch_assoc()) {
+			$id = $messageRow['id'];
+			$deleteMessages2 = "DELETE FROM messages WHERE id = '$id'";
+			$db->query($deleteMessages2);
+		}
+		
+		$delete0 = "DELETE FROM conversation WHERE sUsername = '$username' OR rUsername = '$username'";
 		$delete1 = "DELETE FROM ugames WHERE username = '$username'";
 		$delete2 = "DELETE FROM uachievements WHERE username = '$username'";
 		$delete3 = "DELETE FROM friendrequest WHERE user1 = '$username' OR user2 = '$username'";
 		$delete4 = "DELETE FROM friends WHERE user1 = '$username' OR user2 = '$username'";
 		$delete5 = "DELETE FROM users WHERE username = '$username'";
 		
+		$db->query($delete0);
 		$db->query($delete1);
 		$db->query($delete2);
 		$db->query($delete3);
